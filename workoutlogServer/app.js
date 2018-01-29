3849f888c0c5692856be110cb1a02c164aa6d5ef
Andres Martin <anrumara@gmail.com>
2018-01-29 16:52:31 -0500

    added user and db files

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,48 +1,31 @@
-let express = require('express');
-let app = express();
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
+var sequelize = require('./db.js');
+var User = sequelize.import('./models/user');
 
+User.sync(); // sync( {force: true}) WARNING: This will DROP the table!
+
+app.use(bodyParser.json());
 
 app.use(require('./middleware/headers'));
 
 app.use('/api/test', function (req, res) {
-    res.send("Anything you want, you got it!!");
+    res.send("Hello World!!");
 });
-
-
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('Workoutlog', 'postgres', 'pgDB@1150', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
-
-sequelize.authenticate().then(
-    function () {
-        console.log('connected to workoutlog postgres db');
-    },
-    function (err) {
-        console.log(err);
-    }
-);
 
 
 app.listen(3000, function () {
     console.log("app is listening on 3000");
 });
 
-// build a user model in sqllize
-var User = sequelize.define('user', {
-    username: Sequelize.STRING,
-    passwordhash: Sequelize.STRING,
-});
 
-User.sync();
 
-app.use(bodyParser.json());
 
 app.post('/api/user', function (req, res) {
     var username = req.body.user.username;
     var pass = req.body.user.password;
+    
     User.create({
         username: username,
         passwordhash: ""
@@ -65,11 +48,3 @@ app.post('/api/user', function (req, res) {
 
 //Need to create a user object and use sequelize to put that user into
 //our database.
-
-
-// =============================================
-// *********************************************
-// DANGER THIS WILL DROP (DELETE) THE USER TABLE
-// User.sync({ force: true });
-// *********************************************
-// =============================================
