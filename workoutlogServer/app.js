13ccc852045c0e92a80b974d70336b90213f0432
Andres Martin <anrumara@gmail.com>
2018-01-30 09:16:10 -0500

    added routes

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -7,9 +7,8 @@ var User = sequelize.import('./models/user');
 User.sync(); // sync( {force: true}) WARNING: This will DROP the table!
 
 app.use(bodyParser.json());
-
 app.use(require('./middleware/headers'));
-
+app.use('/api/user', require('./routes/user'));
 app.use('/api/test', function (req, res) {
     res.send("Hello World!!");
 });
@@ -18,33 +17,6 @@ app.use('/api/test', function (req, res) {
 app.listen(3000, function () {
     console.log("app is listening on 3000");
 });
-
-
-
-
-app.post('/api/user', function (req, res) {
-    var username = req.body.user.username;
-    var pass = req.body.user.password;
-    
-    User.create({
-        username: username,
-        passwordhash: ""
-    }).then(
-        //Sequelize is going to return the object it created from db.
-
-        function createSuccess(user) {
-            res.json({
-                user: user,
-                message: 'create'
-            });
-
-        },
-        function createError(err) {
-            res.send(500, err.message);
-        }
-    );
-});
-
 
 //Need to create a user object and use sequelize to put that user into
 //our database.
